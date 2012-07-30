d064253d0b2d8625c079f08f37a57d457936d717
Bojana <bojanajam@gmail.com>
2012-07-30 11:43:05 -0700

    Bare Bones done

---
+++ app/controllers/polls_controller.rb
@@ -0,0 +1,22 @@
+class PollsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+end
